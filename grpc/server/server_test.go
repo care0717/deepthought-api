fa30adc216713d0cadf14210d56dcba4692bcd8b
care0717 <dev.care0717@gmail.com>
2021-07-16 14:18:47 +0900

    test: add server Infer test

---
+++ grpc/server/server_test.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"context"
+	"github.com/care0717/deepthought-api/grpc/proto/deepthought"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestServer_Infer(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		req *deepthought.InferRequest
+	}
+	var (
+		tests = []struct {
+			name    string
+			args    args
+			want    *deepthought.InferResponse
+			wantErr bool
+		}{
+			{
+				name: "Life",
+				args: args{
+					ctx: context.TODO(),
+					req: &deepthought.InferRequest{
+						Query: "Life",
+					},
+				},
+				want:    &deepthought.InferResponse{Answer: 42},
+				wantErr: false,
+			},
+			{
+				name: "Universe",
+				args: args{
+					ctx: context.TODO(),
+					req: &deepthought.InferRequest{
+						Query: "Universe",
+					},
+				},
+				want:    &deepthought.InferResponse{Answer: 42},
+				wantErr: false,
+			},
+			{
+				name: "Everything",
+				args: args{
+					ctx: context.TODO(),
+					req: &deepthought.InferRequest{
+						Query: "Everything",
+					},
+				},
+				want:    &deepthought.InferResponse{Answer: 42},
+				wantErr: false,
+			},
+			{
+				name: "other",
+				args: args{
+					ctx: context.TODO(),
+					req: &deepthought.InferRequest{
+						Query: "hogehoge",
+					},
+				},
+				want:    nil,
+				wantErr: true,
+			},
+			{
+				name: "deadline",
+				args: args{
+					ctx: func() context.Context {
+						ctx, _ := context.WithTimeout(context.TODO(), 1*time.Millisecond)
+						return ctx
+					}(),
+					req: &deepthought.InferRequest{
+						Query: "Life",
+					},
+				},
+				want:    nil,
+				wantErr: true,
+			},
+		}
+	)
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			s := &Server{}
+			got, err := s.Infer(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Infer() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Infer() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
