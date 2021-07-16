88afbbd6fb80bb73f078d9cce3f43ed39a7ccaa3
care0717 <dev.care0717@gmail.com>
2021-07-16 14:50:38 +0900

    feat: add logging and metrics

--- grpc/go.mod
+++ grpc/go.mod
@@ -3,8 +3,12 @@ module github.com/care0717/deepthought-api/grpc
 go 1.16
 
 require (
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/protobuf v1.5.2
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
+	github.com/prometheus/client_golang v1.11.0
 	github.com/spf13/cobra v1.2.1
+	go.uber.org/zap v1.18.1
 	google.golang.org/grpc v1.39.0
 	google.golang.org/protobuf v1.27.1
 )
--- grpc/server/main.go
+++ grpc/server/main.go
@@ -3,29 +3,59 @@ package main
 import (
 	"fmt"
 	"github.com/care0717/deepthought-api/grpc/proto/deepthought"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/keepalive"
 	"net"
+	"net/http"
 	"os"
 	"time"
 )
 
-const portNumber = 13333
+const (
+	portNumber = 13333
+	promPort   = 18888
+)
 
 func main() {
 	kep := keepalive.EnforcementPolicy{
 		MinTime: 10 * time.Second,
 	}
-	serv := grpc.NewServer(grpc.KeepaliveEnforcementPolicy(kep))
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
+	serv := grpc.NewServer(
+		grpc.KeepaliveEnforcementPolicy(kep),
+		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
+			grpc_prometheus.StreamServerInterceptor,
+			grpc_zap.StreamServerInterceptor(logger),
+		)),
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			grpc_prometheus.UnaryServerInterceptor,
+			grpc_zap.UnaryServerInterceptor(logger),
+		)),
+	)
 	deepthought.RegisterComputeServer(serv, &Server{})
-
+	grpc_prometheus.Register(serv)
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.Handler())
+	go func() {
+		logger.Info(fmt.Sprintf("prometheus metrics bind port: %d", promPort))
+		logger.Fatal(fmt.Sprintf("listen failed: %v", http.ListenAndServe(fmt.Sprintf(":%d", promPort), mux)))
+	}()
 	// 待ち受けソケットを作成
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", portNumber))
 	if err != nil {
-		fmt.Println("failed to listen:", err)
-		os.Exit(1)
+		logger.Fatal(fmt.Sprintf("failed to listen: %v", err))
 	}
+	logger.Info(fmt.Sprintf("listen port :%d", portNumber))
 
 	serv.Serve(l)
 }
